f0e83454b414fdf2da2d34caf7c2cac311a5760c
Marcelo Pitol <mrpitol@outlook.com>
2021-06-10 21:06:34 -0300

    Implementado o if simples

--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -14,8 +14,11 @@ public class DesvioSimples {
 		System.out.println("Deixe-me verificar sua idade...");
 		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
 		
+		if(idade>=12) {
+			System.out.println("Você pode jogar");
+		}
 		leitor.close();
-
+		
 	}
 
 }
